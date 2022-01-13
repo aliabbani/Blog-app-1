b6c3c1fa5e1750e0b6cf4cab87e380af8ce9ddf8
ali abbani <aliox@ahlam.com>
2022-01-13 21:34:16 +0200

    Fix rubocop errors

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    before { get "/users" }
+    before { get '/users' }
     it 'returns http success' do
       expect(response).to have_http_status(:ok)
     end
-    
+
     it 'renders index template' do
       expect(response).to render_template('index')
     end
@@ -15,21 +15,17 @@ RSpec.describe 'Users', type: :request do
       expect(response).to_not render_template('show')
     end
 
-    # it 'direct to the correct path' do
-    #   expect(response).to redirect_to('/users') 
-    # end
-    
     it 'check if the response body includes correct placeholder text' do
       expect(response.body).to include('Here is the first page')
     end
   end
 
   describe 'GET /show' do
-    before { get "/users/1" }
+    before { get '/users/1' }
     it 'returns http success' do
       expect(response).to have_http_status(:ok)
     end
-    
+
     it 'renders show template' do
       expect(response).to render_template('show')
     end
@@ -38,10 +34,6 @@ RSpec.describe 'Users', type: :request do
       expect(response).to_not render_template('index')
     end
 
-    # it 'direct to the correct path' do
-    #   expect(response).to redirect_to('/users/:id') 
-    # end
-    
     it 'check if the response body includes correct placeholder text' do
       expect(response.body).to include('Users#show')
     end
